48b7db2b56e615eca9c8fe7cbe4509d57f28a070
dawanlago <dawanlago1@gmail.com>
2023-12-10 15:35:48 -0300

    [feat] post da redação

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ import SimulatorController from './controllers/Simulator/SimulatorController';
 import UserControllers from './controllers/User/UserControllers';
 import { authenticateToken } from './middlewares/authenticateToken';
 import SimulatorAdmController from './controllers/Simulator/SimulatorAdmController';
+import EssayControllers from './controllers/Essay/EssayControllers';
 
 initializeApp(config.firebaseConfig);
 const storage = getStorage();
@@ -99,6 +100,10 @@ routes.get('/verify/:verifyToken', UserControllers.verify);
 routes.get('/users/:company', companyExist, authenticateToken, UserControllers.index);
 routes.put('/users/:user_id/', companyExist, authenticateToken, UserControllers.update);
 routes.delete('/users/:user_id/', companyExist, authenticateToken, UserControllers.destroy);
+
+// Essays
+routes.post('/essays', companyExist, authenticateToken, EssayControllers.store);
+
 // PDF
 routes.post('/upload-pdf', upload.single('pdf'), async (req, res) => {
   try {
