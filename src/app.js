6123b1fd382c0e9f035224faa538751131b2a51b
dawanlago <dawanlago1@gmail.com>
2023-12-10 15:50:27 -0300

    [feat] cors config

--- src/app.js
+++ src/app.js
@@ -16,8 +16,17 @@ class App {
     this.routes();
   }
 
+  options = [
+    cors({
+      origin: '*',
+      methods: '*',
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    }),
+  ];
+
   middlewares() {
-    this.server.use(cors());
+    this.server.use(options);
     this.server.use(express.static('../../../public'));
     this.server.use(bodyParser.json({ limit: '50mb' }));
     this.server.use(express.json());
