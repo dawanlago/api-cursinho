94a4c1643d1ca6248367940d5a2bfacd935d6d8b
dawanlago <dawanlago1@gmail.com>
2024-06-27 11:48:43 -0300

    [fix] deletando timeline antes de gravar

--- src/controllers/Timeline/TimelineController.js
+++ src/controllers/Timeline/TimelineController.js
@@ -1,27 +1,28 @@
 import Timeline from '../../models/Timeline';
 
 class TimelineController {
-    async index(req, res) {
-        const { company } = req.params;
-        const { user } = req.query;
+  async index(req, res) {
+    const { company } = req.params;
+    const { user } = req.query;
 
-        const timeline = await Timeline.findOne({
-                user: user,
-            })
-            .populate('company');
-        return res.json(timeline);
-    }
+    const timeline = await Timeline.findOne({
+      user: user,
+    }).populate('company');
+    return res.json(timeline);
+  }
 
-    async store(req, res) {
-        const { user, days, company } = req.body;
+  async store(req, res) {
+    const { user, days, company } = req.body;
 
-        const timeline = await Timeline.create({
-            user,
-            days,
-            company,
-        });
-        return res.json(timeline);
-    }
+    await Timeline.findByIdAndDelete({ user: user, company });
+
+    const timeline = await Timeline.create({
+      user,
+      days,
+      company,
+    });
+    return res.json(timeline);
+  }
 }
 
 export default new TimelineController();
